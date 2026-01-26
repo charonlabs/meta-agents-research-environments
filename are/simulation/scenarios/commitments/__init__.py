4dd02a5942cd54468bf857c44ac86d33ec90dea9
neoncodes <41237443+charleywolf@users.noreply.github.com>
2026-01-26 11:56:06 -0500

    Add tutorial link to commitment tracking scenario documentation

--- are/simulation/scenarios/commitments/__init__.py
+++ are/simulation/scenarios/commitments/__init__.py
@@ -10,3 +10,4 @@ from are.simulation.scenarios.commitments.scenario import (
 __all__ = ["CommitmentTrackingScenario"]
 
 # are-run -s commitment_tracking -a responses --model gpt-5-mini --model_provider openai
+# https://facebookresearch.github.io/meta-agents-research-environments/tutorials/working_with_scenarios.html
